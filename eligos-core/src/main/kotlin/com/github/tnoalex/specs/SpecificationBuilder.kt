754baa75acf1aa26d77f000c8e359d758abd68e4
Nicholas Alex <millstom2@outlook.com>
2024-05-24 20:46:46 +0800

    <feature> 优化参数解析

---
+++ eligos-core/src/main/kotlin/com/github/tnoalex/specs/SpecificationBuilder.kt
@@ -0,0 +1,45 @@
+package com.github.tnoalex.specs
+
+import com.github.tnoalex.utils.creatDataClassAndFillProperty
+import kotlin.reflect.KClass
+
+class SpecificationBuilder(private val rawArgs: HashMap<String, Any?>) {
+    private var currentArtifact: Any? = null
+
+    fun <T> setProperty(key: String, value: T): SpecificationBuilder {
+        checkPropertyKey(key)
+        rawArgs[key] = value
+        return this
+    }
+
+    fun withCurrentArtifact(key: String): SpecificationBuilder {
+        checkPropertyKey(key)
+        rawArgs[key] = currentArtifact
+        return this
+    }
+
+    fun withPartOfCurrentArtifact(key: String, mapper: (Any) -> Any?): SpecificationBuilder {
+        return setProperty(key, mapper(currentArtifact!!))
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    fun <T> build(): T? {
+        return currentArtifact as? T
+    }
+
+    fun next(targetClazz: KClass<*>): SpecificationBuilder {
+        currentArtifact = creatDataClassAndFillProperty(rawArgs, targetClazz)
+        return this
+    }
+
+    fun <T : Any> next(targetClazz: KClass<T>, builder: (KClass<T>) -> T): SpecificationBuilder {
+        currentArtifact = builder(targetClazz)
+        return this
+    }
+
+    private fun checkPropertyKey(key: String) {
+        if (key in rawArgs) {
+            throw IllegalArgumentException("The specification key '$key' is already in use.")
+        }
+    }
+}
