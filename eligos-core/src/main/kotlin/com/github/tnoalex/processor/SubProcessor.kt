9ba03e8efef8e3bcc3d796a30240b33c7438133c
PiggyCare <PiggyCare@noreply.gitcode.com>
2026-04-22 10:45:42 +0800

    <refactor>: 重构子处理器架构，移除KaaProcessor并修改SubProcessor接口

--- eligos-core/src/main/kotlin/com/github/tnoalex/processor/SubProcessor.kt
+++ eligos-core/src/main/kotlin/com/github/tnoalex/processor/SubProcessor.kt
@@ -1,14 +1,7 @@
 package com.github.tnoalex.processor
 
 import com.intellij.psi.PsiFile
-import org.jetbrains.kotlin.analysis.api.analyze
-import org.jetbrains.kotlin.psi.KtFile
 
 interface SubProcessor : BaseProcessor {
     fun process(psiFile: PsiFile, shareSpace: ShareSpace)
-
-    fun processKaa(ktFile: KtFile, shareSpace: ShareSpace)
-    {
-        process(ktFile as PsiFile, shareSpace)
-    }
 }
