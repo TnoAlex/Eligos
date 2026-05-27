e977da7e9e3782e553d8b6adad1fd4b46edd479e
xyzboom <xyzboom@qq.com>
2026-05-27 09:41:10 +0800

    feat: migrate IgnoredExceptionProcessor

--- eligos-processor/src/main/kotlin/com/github/tnoalex/processor/kotlin/withJava/IgnoredExceptionProcessor.kt
+++ eligos-processor/src/main/kotlin/com/github/tnoalex/processor/kotlin/withJava/IgnoredExceptionProcessor.kt
@@ -10,17 +10,20 @@ import com.github.tnoalex.foundation.language.Language
 import com.github.tnoalex.issues.Severity
 import com.github.tnoalex.issues.kotlin.withJava.IgnoredExceptionIssue
 import com.github.tnoalex.processor.IssueProcessor
-import com.github.tnoalex.processor.utils.*
+import com.github.tnoalex.processor.utils.filePath
+import com.github.tnoalex.processor.utils.refCanNotResolveWarn
+import com.github.tnoalex.processor.utils.startLine
 import com.intellij.psi.*
 import com.intellij.psi.util.PsiTreeUtil
+import org.jetbrains.kotlin.analysis.api.KaExperimentalApi
+import org.jetbrains.kotlin.analysis.api.KaSession
+import org.jetbrains.kotlin.analysis.api.symbols.KaSymbolVisibility
+import org.jetbrains.kotlin.analysis.api.types.KaType
 import org.jetbrains.kotlin.asJava.elements.KtLightElement
-import org.jetbrains.kotlin.js.descriptorUtils.getKotlinTypeFqName
+import org.jetbrains.kotlin.name.ClassId
 import org.jetbrains.kotlin.name.FqName
 import org.jetbrains.kotlin.psi.*
-import org.jetbrains.kotlin.resolve.descriptorUtil.isEffectivelyPublicApi
-import org.jetbrains.kotlin.types.KotlinType
-import org.jetbrains.kotlin.types.typeUtil.supertypes
-import org.jetbrains.kotlin.utils.addToStdlib.ifFalse
+import org.jetbrains.kotlin.types.Variance
 import org.jetbrains.kotlin.utils.addToStdlib.ifTrue
 import org.slf4j.LoggerFactory
 
@@ -31,26 +34,23 @@ class IgnoredExceptionProcessor : IssueProcessor {
     override val severity: Severity = Severity.CODE_SMELL
     override val supportLanguage: List<Language> = listOf(JavaLanguage, KotlinLanguage)
 
-    @EventListener(filterClazz = [KtFile::class,PsiJavaFile::class])
+    @EventListener(filterClazz = [KtFile::class, PsiJavaFile::class])
     override fun process(psiFile: PsiFile) {
         when (psiFile) {
-            is KtFile -> {
-                psiFile.accept(ktApiVisitor)
-            }
-
-            is PsiJavaFile -> {
-                psiFile.accept(javaCallExpressionVisitor)
-            }
+            is KtFile -> psiFile.accept(ktApiVisitor)
+            is PsiJavaFile -> psiFile.accept(javaCallExpressionVisitor)
         }
     }
 
     private val ktApiVisitor = object : KtTreeVisitorVoid() {
         override fun visitNamedFunction(function: KtNamedFunction) {
-            function.resolveToDescriptorIfAny()?.let {
-                if (!it.isEffectivelyPublicApi) return super.visitNamedFunction(function)
-                if (it.annotations.findAnnotation(THROWS_FQ_NAME) != null) return super.visitNamedFunction(function)
-            } ?: super.visitNamedFunction(function)
-            function.accept(ThrowExpressionVisitor(false))
+            analyze {
+                function.symbol.let {
+                    if (it.visibility != KaSymbolVisibility.PUBLIC) return@analyze
+                    if (it.annotations.classIds.contains(THROWS_CLASS_ID)) return@analyze
+                }
+                function.accept(ThrowExpressionVisitor(false))
+            }
             super.visitNamedFunction(function)
         }
     }
@@ -83,7 +83,7 @@ class IgnoredExceptionProcessor : IssueProcessor {
                     val ktOrigin = psiElement.kotlinOrigin ?: return@forEach
                     if (isAnnotatedWithThrows(ktOrigin)) return@forEach
                     ktOrigin.accept(ThrowExpressionVisitor(true))
-                } catch (e: RuntimeException) {
+                } catch (_: RuntimeException) {
                     logger.refCanNotResolveWarn(expression)
                     return@forEach
                 }
@@ -93,24 +93,13 @@ class IgnoredExceptionProcessor : IssueProcessor {
     }
 
     private fun isAnnotatedWithThrows(element: KtElement): Boolean {
-        val declaration = when (element) {
-            is KtConstructor<*> -> {
-                element.resolveToDescriptorIfAny()
-            }
-
-            is KtNamedFunction -> {
-                element.resolveToDescriptorIfAny()
-            }
-
-            is KtProperty -> {
-                element.resolveToDescriptorIfAny()
-            }
-
-            else -> null
+        val symbol = analyze {
+            if (element is KtDeclaration) {
+                element.symbol
+            } else null
         }
-        if (declaration == null) return false
-        if (declaration.annotations.findAnnotation(THROWS_FQ_NAME) != null) return true
-        return false
+        if (symbol == null) return false
+        return symbol.annotations.classIds.contains(THROWS_CLASS_ID)
     }
 
     private inner class ThrowExpressionVisitor(
@@ -118,24 +107,30 @@ class IgnoredExceptionProcessor : IssueProcessor {
     ) : KtTreeVisitorVoid() {
         override fun visitThrowExpression(expression: KtThrowExpression) {
             val throws = expression.thrownExpression ?: return super.visitThrowExpression(expression)
-            val exceptions = expression.bindingContext.getType(throws) ?: let {
-                logger.refCanNotResolveWarn(expression)
-                return super.visitThrowExpression(expression)
+            analyze {
+                val exprType = throws.expressionType ?: let {
+                    logger.refCanNotResolveWarn(expression)
+                    return@analyze
+                }
+                reportIfIsCheckedException(expression, exprType, calledByJava)
             }
-            findCheckedException(expression, exceptions, calledByJava)
             super.visitThrowExpression(expression)
         }
     }
 
-    private fun findCheckedException(element: KtExpression, exception: KotlinType, calledByJava: Boolean) {
-        if (exception.getKotlinTypeFqName(false) == JAVA_RUNTIME_EXCEPTION_FQ_NAME) return
-        exception.supertypes().any { it.getKotlinTypeFqName(false) == JAVA_ERROR }.ifTrue {
-            return
+    @OptIn(KaExperimentalApi::class)
+    private fun KaSession.reportIfIsCheckedException(element: KtExpression, type: KaType, calledByJava: Boolean) {
+        if (type.isClassType(JAVA_RUNTIME_EXCEPTION_CLASS_ID)) return
+        var anySuperIsRuntimeException = false
+        for (superType in type.allSupertypes) {
+            if (superType.isClassType(JAVA_ERROR_CLASS_ID)) return
+            if (superType.isClassType(JAVA_RUNTIME_EXCEPTION_CLASS_ID)) {
+                anySuperIsRuntimeException = true
+                break
+            }
         }
-        exception.supertypes().any {
-            it.getKotlinTypeFqName(false) == JAVA_RUNTIME_EXCEPTION_FQ_NAME
-        }.ifFalse {
-            reportIssue(element, exception.getKotlinTypeFqName(false), calledByJava)
+        if (!anySuperIsRuntimeException) {
+            reportIssue(element, type.render(position = Variance.INVARIANT), calledByJava)
         }
     }
 
@@ -151,7 +146,7 @@ class IgnoredExceptionProcessor : IssueProcessor {
             context.reportIssue(issue)
         } else {
             (context.issues.find { it == issue } as? IgnoredExceptionIssue)?.let {
-                if (calledByJava && !it.calledByJava){
+                if (calledByJava && !it.calledByJava) {
                     it.calledByJava = true
                 }
             }
@@ -160,8 +155,8 @@ class IgnoredExceptionProcessor : IssueProcessor {
 
     companion object {
         private val logger = LoggerFactory.getLogger(IgnoredExceptionProcessor::class.java)
-        private val THROWS_FQ_NAME = FqName("kotlin.jvm.Throws")
-        private const val JAVA_RUNTIME_EXCEPTION_FQ_NAME = "java.lang.RuntimeException"
-        private const val JAVA_ERROR = "java.lang.Error"
+        private val THROWS_CLASS_ID = ClassId(FqName("kotlin.jvm"), FqName("Throws"), false)
+        private val JAVA_RUNTIME_EXCEPTION_CLASS_ID = ClassId(FqName("java.lang"), FqName("RuntimeException"), false)
+        private val JAVA_ERROR_CLASS_ID = ClassId(FqName("java.lang"), FqName("Error"), false)
     }
 }
