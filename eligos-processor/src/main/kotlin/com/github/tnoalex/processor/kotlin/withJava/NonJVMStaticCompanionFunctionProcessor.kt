064fc9ec9137a37d04f23f00f0fc545f55b92ae0
xyzboom <xyzboom@qq.com>
2026-05-27 10:09:58 +0800

    feat: migrate NonJVMStaticCompanionFunctionProcessor

--- eligos-processor/src/main/kotlin/com/github/tnoalex/processor/kotlin/withJava/NonJVMStaticCompanionFunctionProcessor.kt
+++ eligos-processor/src/main/kotlin/com/github/tnoalex/processor/kotlin/withJava/NonJVMStaticCompanionFunctionProcessor.kt
@@ -12,15 +12,14 @@ import com.github.tnoalex.issues.kotlin.withJava.NonJVMStaticCompanionFunctionIs
 import com.github.tnoalex.processor.IssueProcessor
 import com.github.tnoalex.processor.utils.filePath
 import com.github.tnoalex.processor.utils.nameCanNotResolveWarn
-import com.github.tnoalex.processor.utils.resolveToDescriptorIfAny
 import com.github.tnoalex.processor.utils.startLine
 import com.intellij.psi.PsiFile
-import org.jetbrains.kotlin.name.FqName
+import org.jetbrains.kotlin.analysis.api.symbols.KaSymbolVisibility
+import org.jetbrains.kotlin.name.ClassId
 import org.jetbrains.kotlin.psi.KtFile
 import org.jetbrains.kotlin.psi.KtNamedFunction
 import org.jetbrains.kotlin.psi.KtObjectDeclaration
 import org.jetbrains.kotlin.psi.KtTreeVisitorVoid
-import org.jetbrains.kotlin.resolve.descriptorUtil.isEffectivelyPublicApi
 import org.jetbrains.kotlin.utils.addToStdlib.ifFalse
 import org.slf4j.LoggerFactory
 
@@ -45,35 +44,36 @@ class NonJVMStaticCompanionFunctionProcessor : IssueProcessor {
 
     private val namedFunctionVisitorVoid = object : KtTreeVisitorVoid() {
         override fun visitNamedFunction(function: KtNamedFunction) {
-            function.resolveToDescriptorIfAny()?.let {
-                if (!it.isEffectivelyPublicApi) return super.visitNamedFunction(function)
-                if (it.annotations.findAnnotation(FqName(JVM_STATIC_FQ_NAME)) != null)
-                    return super.visitNamedFunction(function)
-            } ?: return super.visitNamedFunction(function)
-            context.reportIssue(
-                NonJVMStaticCompanionFunctionIssue(
-                    function.filePath,
-                    function.fqName?.asString() ?: let {
-                        logger.nameCanNotResolveWarn("function", function)
-                        "unknown function name"
-                    },
-                    function.valueParameters.map {
-                        if (it.fqName != null) it.fqName!!.asString()
-                        else {
-                            logger.nameCanNotResolveWarn("parameter", it)
-                            "unknown parameter name"
-                        }
-                    }.takeIf { it.isNotEmpty() },
-                    function.startLine,
-                    function.text
-                )
-            )
+            analyze {
+                val symbol = function.symbol
+                if (symbol.visibility != KaSymbolVisibility.PUBLIC) return@analyze
+                if (!symbol.annotations.classIds.contains(JVM_STATIC_CLASS_ID)) {
+                    context.reportIssue(
+                        NonJVMStaticCompanionFunctionIssue(
+                            function.filePath,
+                            function.fqName?.asString() ?: let {
+                                logger.nameCanNotResolveWarn("function", function)
+                                "unknown function name"
+                            },
+                            function.valueParameters.map {
+                                if (it.fqName != null) it.fqName!!.asString()
+                                else {
+                                    logger.nameCanNotResolveWarn("parameter", it)
+                                    "unknown parameter name"
+                                }
+                            }.takeIf { it.isNotEmpty() },
+                            function.startLine,
+                            function.text
+                        )
+                    )
+                }
+            }
             super.visitNamedFunction(function)
         }
     }
 
     companion object {
-        private const val JVM_STATIC_FQ_NAME = "kotlin.jvm.JvmStatic"
+        private val JVM_STATIC_CLASS_ID = ClassId.fromString("kotlin/jvm/JvmStatic")
         private val logger = LoggerFactory.getLogger(NonJVMStaticCompanionFunctionProcessor::class.java)
     }
 }
