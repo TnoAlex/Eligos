af01968985880c3eedf553f4824f090ae47b6bb8
xyzboom <xyzboom@qq.com>
2026-05-27 16:19:44 +0800

    tset: clean todo in test data.

--- eligos-cli/src/test/resources/nonnullAssertionOnNullableType/A.java
+++ eligos-cli/src/test/resources/nonnullAssertionOnNullableType/A.java
@@ -1,8 +1,6 @@
 import org.jetbrains.annotations.Nullable;
 
 public class A {
-    // todo: KAA resolve this @Nullable incorrectly into default package
-    //  instead of org.jetbrains.annotations package, which causes the test to fail
     @Nullable
     public static A func() {
         return null;
